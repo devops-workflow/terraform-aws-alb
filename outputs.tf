607cbb30217882ccbbe147bf751724bdf1a9954f
Steven Nemetz <snemetz@hotmail.com>
2018-02-10 07:55:09 -0800

    Add CircleCI. Fix formatting

--- outputs.tf
+++ outputs.tf
@@ -3,24 +3,32 @@
 //
 output "arn" {
   description = "ARN of the LB itself. Useful for debug output, for example when attaching a WAF."
+
   #value       = "${element(concat(aws_lb.this.*.arn, list("")), 0)}"
-  value       = "${element(concat(aws_lb.application.*.arn, aws_lb.network.*.arn, list("")), 0)}"
+  value = "${element(concat(aws_lb.application.*.arn, aws_lb.network.*.arn, list("")), 0)}"
 }
+
 output "dns_name" {
   description = "The DNS name of the LB presumably to be used with a friendlier CNAME."
+
   #value       = "${element(concat(aws_lb.this.*.dns_name, list("")), 0)}"
-  value       = "${element(concat(aws_lb.application.*.dns_name, aws_lb.network.*.dns_name, list("")), 0)}"
+  value = "${element(concat(aws_lb.application.*.dns_name, aws_lb.network.*.dns_name, list("")), 0)}"
 }
+
 output "id" {
   description = "The ID of the LB we created."
+
   #value       = "${element(concat(aws_lb.this.*.id, list("")), 0)}"
-  value       = "${element(concat(aws_lb.application.*.id, aws_lb.network.*.id, list("")), 0)}"
+  value = "${element(concat(aws_lb.application.*.id, aws_lb.network.*.id, list("")), 0)}"
 }
+
 output "zone_id" {
   description = "The zone_id of the LB to assist with creating DNS records."
+
   #value       = "${element(concat(aws_lb.this.*.zone_id, list("")), 0)}"
-  value       = "${element(concat(aws_lb.application.*.zone_id, aws_lb.network.*.zone_id, list("")), 0)}"
+  value = "${element(concat(aws_lb.application.*.zone_id, aws_lb.network.*.zone_id, list("")), 0)}"
 }
+
 # arn_suffix
 # canonical_hosted_zone_id
 
@@ -31,30 +39,37 @@ output "listener_http_arns" {
   description = "The ARNs of the HTTP LB Listeners"
   value       = "${aws_lb_listener.http.*.arn}"
 }
+
 output "listener_http_ids" {
   description = "The IDs of the HTTP LB Listeners"
   value       = "${aws_lb_listener.http.*.id}"
 }
+
 output "listener_https_arns" {
   description = "The ARNs of the HTTPS LB Listeners"
   value       = "${aws_lb_listener.https.*.arn}"
 }
+
 output "listener_https_ids" {
   description = "The IDs of the HTTPS LB Listeners"
   value       = "${aws_lb_listener.https.*.id}"
 }
+
 output "listener_tcp_arns" {
   description = "The ARNs of the network TCP LB Listeners"
   value       = "${aws_lb_listener.network.*.arn}"
 }
+
 output "listener_tcp_ids" {
   description = "The IDs of the network TCP LB Listeners"
   value       = "${aws_lb_listener.network.*.id}"
 }
+
 output "listener_arns" {
   description = "ARNs of all the LB Listeners"
   value       = "${compact(concat(aws_lb_listener.http.*.arn,aws_lb_listener.https.*.arn,aws_lb_listener.network.*.arn))}"
 }
+
 output "listener_ids" {
   description = "IDs of all the LB Listeners"
   value       = "${compact(concat(aws_lb_listener.http.*.id,aws_lb_listener.https.*.id,aws_lb_listener.network.*.id))}"
@@ -67,14 +82,17 @@ output "target_group_http_arns" {
   description = "ARNs of the HTTP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${aws_lb_target_group.application-http.*.arn}"
 }
+
 output "target_group_https_arns" {
   description = "ARNs of the HTTPS target groups. Useful for passing to your Auto Scaling group module."
   value       = "${aws_lb_target_group.application-https.*.arn}"
 }
+
 output "target_group_tcp_arns" {
   description = "ARNs of the TCP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${aws_lb_target_group.network.*.arn}"
 }
+
 output "target_group_arns" {
   description = "ARNs of all the target groups. Useful for passing to your Auto Scaling group module."
   value       = "${compact(concat(aws_lb_target_group.application-http.*.arn,aws_lb_target_group.application-https.*.arn,aws_lb_target_group.network.*.arn))}"
@@ -84,14 +102,17 @@ output "target_group_http_ids" {
   description = "IDs of the HTTP target groups"
   value       = "${aws_lb_target_group.application-http.*.id}"
 }
+
 output "target_group_https_ids" {
   description = "IDs of the HTTPS target groups"
   value       = "${aws_lb_target_group.application-https.*.id}"
 }
+
 output "target_group_tcp_ids" {
   description = "IDs of the TCP target groups"
   value       = "${aws_lb_target_group.network.*.id}"
 }
+
 output "target_group_ids" {
   description = "IDs of all the target groups"
   value       = "${compact(concat(aws_lb_target_group.application-http.*.id,aws_lb_target_group.application-https.*.id,aws_lb_target_group.network.*.id))}"
--- test/outputs.tf
+++ test/outputs.tf
@@ -14,18 +14,22 @@ output "tcp_arn" {
   description = "ARN of the LB itself. Useful for debug output, for example when attaching a WAF."
   value       = "${module.lb-tcp.arn}"
 }
+
 output "tcp_dns_name" {
   description = "The DNS name of the LB presumably to be used with a friendlier CNAME."
   value       = "${module.lb-tcp.dns_name}"
 }
+
 output "tcp_id" {
   description = "The ID of the LB we created."
   value       = "${module.lb-tcp.id}"
 }
+
 output "tcp_zone_id" {
   description = "The zone_id of the LB to assist with creating DNS records."
   value       = "${module.lb-tcp.zone_id}"
 }
+
 # arn_suffix
 # canonical_hosted_zone_id
 
@@ -36,30 +40,37 @@ output "tcp_listener_http_arns" {
   description = "The ARNs of the HTTP LB Listeners"
   value       = "${module.lb-tcp.listener_http_arns}"
 }
+
 output "tcp_listener_http_ids" {
   description = "The IDs of the HTTP LB Listeners"
   value       = "${module.lb-tcp.listener_http_ids}"
 }
+
 output "tcp_listener_https_arns" {
   description = "The ARNs of the HTTPS LB Listeners"
   value       = "${module.lb-tcp.listener_https_arns}"
 }
+
 output "tcp_listener_https_ids" {
   description = "The IDs of the HTTPS LB Listeners"
   value       = "${module.lb-tcp.listener_https_ids}"
 }
+
 output "tcp_listener_tcp_arns" {
   description = "The ARNs of the network TCP LB Listeners"
   value       = "${module.lb-tcp.listener_tcp_arns}"
 }
+
 output "tcp_listener_tcp_ids" {
   description = "The IDs of the network TCP LB Listeners"
   value       = "${module.lb-tcp.listener_tcp_ids}"
 }
+
 output "tcp_listener_arns" {
   description = "ARNs of all the LB Listeners"
   value       = "${module.lb-tcp.listener_arns}"
 }
+
 output "tcp_listener_ids" {
   description = "IDs of all the LB Listeners"
   value       = "${module.lb-tcp.listener_ids}"
@@ -72,34 +83,42 @@ output "tcp_target_group_http_arns" {
   description = "ARNs of the HTTP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-tcp.target_group_http_arns}"
 }
+
 output "tcp_target_group_https_arns" {
   description = "ARNs of the HTTPS target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-tcp.target_group_https_arns}"
 }
+
 output "tcp_target_group_tcp_arns" {
   description = "ARNs of the TCP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-tcp.target_group_tcp_arns}"
 }
+
 output "tcp_target_group_arns" {
   description = "ARNs of all the target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-tcp.target_group_arns}"
 }
+
 output "tcp_target_group_http_ids" {
   description = "IDs of the HTTP target groups"
   value       = "${module.lb-tcp.target_group_http_ids}"
 }
+
 output "tcp_target_group_https_ids" {
   description = "IDs of the HTTPS target groups"
   value       = "${module.lb-tcp.target_group_https_ids}"
 }
+
 output "tcp_target_group_tcp_ids" {
   description = "IDs of the TCP target groups"
   value       = "${module.lb-tcp.target_group_tcp_ids}"
 }
+
 output "tcp_target_group_ids" {
   description = "IDs of all the target groups"
   value       = "${module.lb-tcp.target_group_ids}"
 }
+
 # arn_suffix
 # name
 
@@ -111,7 +130,6 @@ output "tcp_principal_account_id" {
   value       = "${module.lb-tcp.principal_account_id}"
 }
 
-
 //// LB HTTP
 //
 // LB attributes
@@ -120,18 +138,22 @@ output "http_arn" {
   description = "ARN of the LB itself. Useful for debug output, for example when attaching a WAF."
   value       = "${module.lb-http.arn}"
 }
+
 output "http_dns_name" {
   description = "The DNS name of the LB presumably to be used with a friendlier CNAME."
   value       = "${module.lb-http.dns_name}"
 }
+
 output "http_id" {
   description = "The ID of the LB we created."
   value       = "${module.lb-http.id}"
 }
+
 output "http_zone_id" {
   description = "The zone_id of the LB to assist with creating DNS records."
   value       = "${module.lb-http.zone_id}"
 }
+
 # arn_suffix
 # canonical_hosted_zone_id
 
@@ -142,30 +164,37 @@ output "http_listener_http_arns" {
   description = "The ARNs of the HTTP LB Listeners"
   value       = "${module.lb-http.listener_http_arns}"
 }
+
 output "http_listener_http_ids" {
   description = "The IDs of the HTTP LB Listeners"
   value       = "${module.lb-http.listener_http_ids}"
 }
+
 output "http_listener_https_arns" {
   description = "The ARNs of the HTTPS LB Listeners"
   value       = "${module.lb-http.listener_https_arns}"
 }
+
 output "http_listener_https_ids" {
   description = "The IDs of the HTTPS LB Listeners"
   value       = "${module.lb-http.listener_https_ids}"
 }
+
 output "http_listener_tcp_arns" {
   description = "The ARNs of the network TCP LB Listeners"
   value       = "${module.lb-http.listener_tcp_arns}"
 }
+
 output "http_listener_tcp_ids" {
   description = "The IDs of the network TCP LB Listeners"
   value       = "${module.lb-http.listener_tcp_ids}"
 }
+
 output "http_listener_arns" {
   description = "ARNs of all the LB Listeners"
   value       = "${module.lb-http.listener_arns}"
 }
+
 output "http_listener_ids" {
   description = "IDs of all the LB Listeners"
   value       = "${module.lb-http.listener_ids}"
@@ -178,34 +207,42 @@ output "http_target_group_http_arns" {
   description = "ARNs of the HTTP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-http.target_group_http_arns}"
 }
+
 output "http_target_group_https_arns" {
   description = "ARNs of the HTTPS target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-http.target_group_https_arns}"
 }
+
 output "http_target_group_tcp_arns" {
   description = "ARNs of the TCP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-http.target_group_tcp_arns}"
 }
+
 output "http_target_group_arns" {
   description = "ARNs of all the target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-http.target_group_arns}"
 }
+
 output "http_target_group_http_ids" {
   description = "IDs of the HTTP target groups"
   value       = "${module.lb-http.target_group_http_ids}"
 }
+
 output "http_target_group_https_ids" {
   description = "IDs of the HTTPS target groups"
   value       = "${module.lb-http.target_group_https_ids}"
 }
+
 output "http_target_group_tcp_ids" {
   description = "IDs of the TCP target groups"
   value       = "${module.lb-http.target_group_tcp_ids}"
 }
+
 output "http_target_group_ids" {
   description = "IDs of all the target groups"
   value       = "${module.lb-http.target_group_ids}"
 }
+
 # arn_suffix
 # name
 
@@ -225,18 +262,22 @@ output "https_arn" {
   description = "ARN of the LB itself. Useful for debug output, for example when attaching a WAF."
   value       = "${module.lb-https.arn}"
 }
+
 output "https_dns_name" {
   description = "The DNS name of the LB presumably to be used with a friendlier CNAME."
   value       = "${module.lb-https.dns_name}"
 }
+
 output "https_id" {
   description = "The ID of the LB we created."
   value       = "${module.lb-https.id}"
 }
+
 output "https_zone_id" {
   description = "The zone_id of the LB to assist with creating DNS records."
   value       = "${module.lb-https.zone_id}"
 }
+
 # arn_suffix
 # canonical_hosted_zone_id
 
@@ -247,30 +288,37 @@ output "https_listener_http_arns" {
   description = "The ARNs of the HTTP LB Listeners"
   value       = "${module.lb-https.listener_http_arns}"
 }
+
 output "https_listener_http_ids" {
   description = "The IDs of the HTTP LB Listeners"
   value       = "${module.lb-https.listener_http_ids}"
 }
+
 output "https_listener_https_arns" {
   description = "The ARNs of the HTTPS LB Listeners"
   value       = "${module.lb-https.listener_https_arns}"
 }
+
 output "https_listener_https_ids" {
   description = "The IDs of the HTTPS LB Listeners"
   value       = "${module.lb-https.listener_https_ids}"
 }
+
 output "https_listener_tcp_arns" {
   description = "The ARNs of the network TCP LB Listeners"
   value       = "${module.lb-https.listener_tcp_arns}"
 }
+
 output "https_listener_tcp_ids" {
   description = "The IDs of the network TCP LB Listeners"
   value       = "${module.lb-https.listener_tcp_ids}"
 }
+
 output "https_listener_arns" {
   description = "ARNs of all the LB Listeners"
   value       = "${module.lb-https.listener_arns}"
 }
+
 output "https_listener_ids" {
   description = "IDs of all the LB Listeners"
   value       = "${module.lb-https.listener_ids}"
@@ -283,34 +331,42 @@ output "https_target_group_http_arns" {
   description = "ARNs of the HTTP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-https.target_group_http_arns}"
 }
+
 output "https_target_group_https_arns" {
   description = "ARNs of the HTTPS target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-https.target_group_https_arns}"
 }
+
 output "https_target_group_tcp_arns" {
   description = "ARNs of the TCP target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-https.target_group_tcp_arns}"
 }
+
 output "https_target_group_arns" {
   description = "ARNs of all the target groups. Useful for passing to your Auto Scaling group module."
   value       = "${module.lb-https.target_group_arns}"
 }
+
 output "https_target_group_http_ids" {
   description = "IDs of the HTTP target groups"
   value       = "${module.lb-https.target_group_http_ids}"
 }
+
 output "https_target_group_https_ids" {
   description = "IDs of the HTTPS target groups"
   value       = "${module.lb-https.target_group_https_ids}"
 }
+
 output "https_target_group_tcp_ids" {
   description = "IDs of the TCP target groups"
   value       = "${module.lb-https.target_group_tcp_ids}"
 }
+
 output "https_target_group_ids" {
   description = "IDs of all the target groups"
   value       = "${module.lb-https.target_group_ids}"
 }
+
 # arn_suffix
 # name
 
